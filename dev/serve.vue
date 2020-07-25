d49a166d87b5f38f07a9fd1f86e9e5f7d28f852e
Chris Doty <dotyc@civicinc.com>
2020-07-24 23:37:57 -0500

    fix: first commit

---
+++ dev/serve.vue
@@ -0,0 +1,17 @@
+<script>
+import Vue from 'vue';
+import CodigoPropertyPanel from '@/codigo-property-panel.vue';
+
+export default Vue.extend({
+  name: 'ServeDev',
+  components: {
+    CodigoPropertyPanel
+  }
+});
+</script>
+
+<template>
+  <div id="app">
+    <codigo-property-panel />
+  </div>
+</template>
